eb0e2acdd01baba4d04da6ac74276856acb7904c
Leovianaf <leonardoviana.nave@gmail.com>
2024-07-24 10:25:09 -0300

    feat: Postgre seted and table user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// console.log(`${__dirname}/**/*.entity{.js,.ts}`);
+// console.log(`${__dirname}/../src/**/*.entity{.js,.ts}`);
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      port: parseInt(process.env.DB_PORT),
+      // autoLoadEntities: true,
+      type: 'postgres',
+      synchronize: true,
+      entities: [`${__dirname}/**/*.entity{.js,.ts}`],
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,30 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
-import { User } from './interface/user.interface';
+import { UserEntity } from './interface/user.entity';
 import { hash } from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  private users: User[] = [];
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
 
-  async createUser(createUserDto: CreateUserDto): Promise<User> {
+  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
     const saltOrRounds = 10;
     const passwordHashed = await hash(createUserDto.password, saltOrRounds);
 
-    console.log('passwordHashed', passwordHashed);
-
-    const user = {
+    return this.userRepository.save({
       ...createUserDto,
-      id: this.users.length + 1,
       password: passwordHashed,
-    };
-
-    this.users.push(user);
-
-    return user;
+    });
   }
 
-  async getAllUsers(): Promise<User[]> {
-    return this.users;
+  async getAllUsers(): Promise<UserEntity[]> {
+    return this.userRepository.find();
   }
 }
