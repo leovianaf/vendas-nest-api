ed0a4b1ae1b27efd85c496ee3843edeeca97de4c
Leovianaf <leonardoviana.nave@gmail.com>
2024-08-09 13:54:51 -0300

    feat: dtos city and state

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -26,7 +26,13 @@ export class UserService {
   async getUserByIdUsingRelations(userId: number): Promise<UserEntity> {
     return this.userRepository.findOne({
       where: { id: userId },
-      relations: ['addresses'],
+      relations: {
+        addresses: {
+          city: {
+            state: true,
+          },
+        },
+      },
     });
   }
 
