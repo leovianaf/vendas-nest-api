fdaf84fb014535f76531dd5d1546ffedadc18226
Leovianaf <leonardoviana.nave@gmail.com>
2024-08-15 14:43:53 -0300

    feat: insert validation jwt

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -9,11 +9,14 @@ import {
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dtos/createAddress.dto';
 import { AddressEntity } from './entities/address.entity';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserType } from 'src/user/enum/user-type.enum';
 
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
+  @Roles(UserType.User)
   @Post('/:userId')
   @UsePipes(ValidationPipe)
   async createAddress(
