8b0afc79b7702c660657910815d17fa01c0baece
Leovianaf <leonardoviana.nave@gmail.com>
2024-08-19 13:33:51 -0300

    feat:create decorator userId

---
+++ src/decorators/user-id.decorator.ts
@@ -0,0 +1,11 @@
+import { createParamDecorator } from '@nestjs/common';
+import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
+import { authorizationToLoginPayload } from 'src/utils/base-64-converter';
+
+export const UserId = createParamDecorator((_, ctx: ExecutionContextHost) => {
+  const { authorization } = ctx.switchToHttp().getRequest().headers;
+
+  const loginPayload = authorizationToLoginPayload(authorization);
+
+  return loginPayload?.id;
+});
